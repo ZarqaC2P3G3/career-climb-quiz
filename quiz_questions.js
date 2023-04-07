23c49b064f063dd07f730ad72739bb4cbc358d4d
MajdFoudeh <majdfoude@gmail.com>
2023-04-07 12:49:32 +0300

    add the quiz questions

---
+++ quiz_questions.js
@@ -0,0 +1,322 @@
+const quizzes = {
+    javascriptQuiz: {
+        questions: [
+            {
+                title: "What is JavaScript?",
+                answers: {
+                    0: " JavaScript is a scripting language used to make the website interactive",
+                    1: "JavaScript is an assembly language used to make the website interactive",
+                    2: "JavaScript is a compiled language used to make the website interactive",
+                    3: "None of the mentioned",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title: "Which of the following is correct about JavaScript?",
+                answers: {
+                    0: "JavaScript is an Object-Based language",
+                    1: "JavaScript is Assembly-language",
+                    2: "JavaScript is an Object-Oriented language",
+                    3: " JavaScript is a High-level language",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title:
+                    "Arrays in JavaScript are defined by which of the following statements?",
+                answers: {
+                    0: "It is an ordered list of values",
+                    1: "It is an ordered list of objects",
+                    2: "It is an ordered list of string",
+                    3: "It is an ordered list of functions",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title:
+                    "Where is Client-side JavaScript code is embedded within HTML documents?",
+                answers: {
+                    0: "A URL that uses the special javascript:code",
+                    1: "A URL that uses the special javascript:protocol",
+                    2: "A URL that uses the special javascript:encoding",
+                    3: "A URL that uses the special javascript:stack",
+                },
+                correctAnswer: 1,
+            },
+            {
+                title: "Which of the following is correct about JavaScript?",
+                answers: {
+                    0: "JavaScript is an Object-Based language",
+                    1: "JavaScript is Assembly-language",
+                    2: "JavaScript is an Object-Oriented language",
+                    3: " JavaScript is a High-level language",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title: "Which of the following is correct about JavaScript?",
+                answers: {
+                    0: "JavaScript is an Object-Based language",
+                    1: "JavaScript is Assembly-language",
+                    2: "JavaScript is an Object-Oriented language",
+                    3: " JavaScript is a High-level language",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title: "Which of the following is correct about JavaScript?",
+                answers: {
+                    0: "JavaScript is an Object-Based language",
+                    1: "JavaScript is Assembly-language",
+                    2: "JavaScript is an Object-Oriented language",
+                    3: " JavaScript is a High-level language",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title: "Which of the following is correct about JavaScript?",
+                answers: {
+                    0: "JavaScript is an Object-Based language",
+                    1: "JavaScript is Assembly-language",
+                    2: "JavaScript is an Object-Oriented language",
+                    3: " JavaScript is a High-level language",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title: "Which of the following is correct about JavaScript?",
+                answers: {
+                    0: "JavaScript is an Object-Based language",
+                    1: "JavaScript is Assembly-language",
+                    2: "JavaScript is an Object-Oriented language",
+                    3: " JavaScript is a High-level language",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title: "Which of the following is correct about JavaScript?",
+                answers: {
+                    0: "JavaScript is an Object-Based language",
+                    1: "JavaScript is Assembly-language",
+                    2: "JavaScript is an Object-Oriented language",
+                    3: " JavaScript is a High-level language",
+                },
+                correctAnswer: 0,
+            },
+        ],
+    },
+
+    htmlQuiz: {
+        questions: [
+            {
+                title: "What is HTML?",
+                answers: {
+                    0: "HTML describes the structure of a webpage",
+                    1: "HTML is the standard markup language mainly used to create web pages",
+                    2: "HTML consists of a set of elements that helps the browser how to view the content",
+                    3: "All of the mentioned",
+                },
+                correctAnswer: 3,
+            },
+            {
+                title: "Who is the father of HTML?",
+                answers: {
+                    0: "Rasmus Lerdorf",
+                    1: "Tim Berners-Lee",
+                    2: "Brendan Eich",
+                    3: "Sergey Brin",
+                },
+                correctAnswer: 1,
+            },
+            {
+                title:
+                    "HTML stands for __________",
+                answers: {
+                    0: "HyperText Markup Language",
+                    1: "HyperText Machine Language",
+                    2: "HyperText Marking Language",
+                    3: " HighText Marking Language",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title:
+                    "What is the correct syntax of doctype in HTML5?",
+                answers: {
+                    0: "</doctype html>",
+                    1: "<doctype html>",
+                    2: "<doctype html!>",
+                    3: "<!doctype html>",
+                },
+                correctAnswer: 3,
+            },
+            {
+                title: "Which of the following is used to read an HTML page and render it?",
+                answers: {
+                    0: "Web server",
+                    1: "Web network",
+                    2: "Web browser",
+                    3: "Web matrix",
+                },
+                correctAnswer: 2,
+            },
+            {
+                title: "Which of the following is not a difference between HTML and XHTML?",
+                answers: {
+                    0: "Charset in both html and xhtml is “text/html”",
+                    1: "JavaScript is Assembly-languageTags and attributes are case-insensitive in HTML but not in XHTML",
+                    2: "Special characters must be escaped using character entities in XHTML unlike HTML",
+                    3: "Charset in html is “text/html” where as in xhtml it is “application/xml+xhtml”",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title: "Which of the following tag is used for inserting the largest heading in HTML?",
+                answers: {
+                    0: "head",
+                    1: "<h1>",
+                    2: "<h6>",
+                    3: "heading",
+                },
+                correctAnswer: 1,
+            },
+            {
+                title: "What is DOM in HTML?",
+                answers: {
+                    0: "Language dependent application programming",
+                    1: "Hierarchy of objects in ASP.NET",
+                    2: "Application programming interface",
+                    3: " Convention for representing and interacting with objects in html documents",
+                },
+                correctAnswer: 3,
+            },
+            {
+                title: "In which part of the HTML metadata is contained?",
+                answers: {
+                    0: "head tag",
+                    1: "title tag",
+                    2: "html tag",
+                    3: "body tag",
+                },
+                correctAnswer: 0,
+            },
+            {
+                title: "Which element is used to get highlighted text in HTML5?",
+                answers: {
+                    0: "<u>",
+                    1: "<mark>",
+                    2: "<highlight>",
+                    3: "<b>",
+                },
+                correctAnswer: 1,
+            },
+        ],
+    },
+    
+    cssQuiz: {
+        questions: [
+            {
+                title: "What is CSS?",
+                answers: {
+                    0: "CSS is a style sheet language",
+                    1: "CSS is designed to separate the presentation and content, including layout, colors, and fonts",
+                    2: "CSS is the language used to style the HTML documents",
+                    3: "All of the mentioned",
+                },
+                correctAnswer: 3,
+            },
+            {
+                title: "Which of the following tag is used to embed css in html page?",
+                answers: {
+                    0: "<css>",
+                    1: "<!DOCTYPE html>",
+                    2: "<script>",
+                    3: "<style>",
+                },
+                correctAnswer: 3,
+            },
+            {
+                title:
+                    " Which of the following CSS selectors are used to specify a group of elements?",
+                answers: {
+                    0: "tag",
+                    1: "id",
+                    2: "class",
+                    3: "both class and tag",
+                },
+                correctAnswer: 2,
+            },
+            {
+                title:
+                    "Which of the following has introduced text, list, box, margin, border, color, and background properties?",
+                answers: {
+                    0: "HTML",
+                    1: "PHP",
+                    2: "CSS",
+                    3: "Ajax",
+                },
+                correctAnswer: 2,
+            },
+            {
+                title: "Which of the following CSS framework is used to create a responsive design?",
+                answers: {
+                    0: "django",
+                    1: "rails",
+                    2: "larawell",
+                    3: "bootstrap",
+                },
+                correctAnswer: 3,
+            },
+            {
+                title: "Which of the following CSS selector is used to specify a rule to bind a particular unique element?",
+                answers: {
+                    0: "tag",
+                    1: "id",
+                    2: "class",
+                    3: "both class and tag",
+                },
+                correctAnswer: 1,
+            },
+            {
+                title: " Which of the following type of HTML tag is used to define an internal style sheet?",
+                answers: {
+                    0: "<script>",
+                    1: "<Link>",
+                    2: "<class>",
+                    3: "<style>",
+                },
+                correctAnswer: 3,
+            },
+            {
+                title: "Which of the following CSS property is used to make the text bold?",
+                answers: {
+                    0: "text-decoration: bold",
+                    1: "font-weight: bold",
+                    2: "font-style: bold",
+                    3: "text-align: bold",
+                },
+                correctAnswer: 1,
+            },
+            {
+                title: "Which of the following CSS style property is used to specify an italic text?",
+                answers: {
+                    0: "style",
+                    1: "font",
+                    2: "font-style",
+                    3: "@font-face",
+                },
+                correctAnswer: 2,
+            },
+            {
+                title: " Which of the following is the correct syntax to link an external style sheet in the HTML file?",
+                answers: {
+                    0: "<link rel=”stylesheet” href=”style.css” />",
+                    1: "<link rel=”stylesheet” src=”style.css” />",
+                    2: "<style rel=”stylesheet” src=”style.css” />",
+                    3: "<style rel=”stylesheet” link=”style.css” />",
+                },
+                correctAnswer: 0,
+            },
+        ],
+    },
+};
