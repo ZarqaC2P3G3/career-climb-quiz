c5dd0fd1037c11178c2610a8db47528832797f5e
fareskhalil <63003062+faresmohalhel@users.noreply.github.com>
2023-04-08 06:39:42 +0300

    استرها يا رب

--- src/js/welcomePage.js
+++ src/js/welcomePage.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   console.log(user.selectedValue);
   quizImage.setAttribute(
     "src",
-    `../../../img/${user.selectedValue}-logo-new.png`
+    `./../../../img/${user.selectedValue}-logo-new.png`
   );
   quizImage.style.boxShadow = `: 0 0 100px ${dropShadowColor};`;
   positionBriefContainer.textContent = `The ${user.selectedValue} Multiple Choice Quiz is a web-based platform designed to test your knowledge of ${user.selectedValue}. The quiz features a series of multiple-choice questions, covering various topics such as syntax, data types, functions, and more. The questions range in difficulty from beginner to advanced, allowing users to test their skills at various levels.`;
